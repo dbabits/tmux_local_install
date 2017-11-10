17bf9f97af53eb35066b6e562cb1c9fe50e06fdd
dima <dbabits@gmail.com>
2017-11-10 16:17:45 -0500

    --enable-static, no LDFLAGS -static

--- tmux_local_install.sh
+++ tmux_local_install.sh
@@ -6,6 +6,9 @@
 
 # exit on error
 set -e
+rmtempdir=$1
+
+[ -d $HOME/tmux_tmp ]  && rm -rfv $HOME/tmux_tmp 
 
 TMUX_VERSION=2.6
 LIBEVENT_VERSION=2.1.8
@@ -16,9 +19,6 @@ mkdir -p $HOME/local $HOME/tmux_tmp
 cd $HOME/tmux_tmp
 
 # download source files for tmux, libevent, and ncurses
-#wget -O tmux-${TMUX_VERSION}.tar.gz http://sourceforge.net/projects/tmux/files/tmux/tmux-${TMUX_VERSION}/tmux-${TMUX_VERSION}.tar.gz/download
-#wget https://github.com/downloads/libevent/libevent/libevent-2.0.19-stable.tar.gz
-#wget ftp://ftp.gnu.org/gnu/ncurses/ncurses-5.9.tar.gz
 wget https://github.com/tmux/tmux/releases/download/${TMUX_VERSION}/tmux-${TMUX_VERSION}.tar.gz
 wget https://github.com/libevent/libevent/releases/download/release-${LIBEVENT_VERSION}-stable/libevent-${LIBEVENT_VERSION}-stable.tar.gz
 wget ftp://ftp.gnu.org/gnu/ncurses/ncurses-${NCURSES_VERSION}.tar.gz
@@ -40,7 +40,7 @@ cd ..
 ############
 tar xvzf ncurses-${NCURSES_VERSION}.tar.gz
 cd ncurses-${NCURSES_VERSION}
-./configure --prefix=$HOME/local
+./configure  --prefix=$HOME/local
 make
 make install
 cd ..
@@ -50,12 +50,12 @@ cd ..
 ############
 tar xvzf tmux-${TMUX_VERSION}.tar.gz
 cd tmux-${TMUX_VERSION}
-./configure CFLAGS="-I$HOME/local/include -I$HOME/local/include/ncurses" LDFLAGS="-L$HOME/local/lib -L$HOME/local/include/ncurses -L$HOME/local/include"
-CPPFLAGS="-I$HOME/local/include -I$HOME/local/include/ncurses" LDFLAGS="-static -L$HOME/local/include -L$HOME/local/include/ncurses -L$HOME/local/lib" make
+./configure --enable-static CFLAGS="-I$HOME/local/include -I$HOME/local/include/ncurses" LDFLAGS="-L$HOME/local/lib -L$HOME/local/include/ncurses -L$HOME/local/include"
+CPPFLAGS="-I$HOME/local/include -I$HOME/local/include/ncurses" LDFLAGS="-L$HOME/local/include -L$HOME/local/include/ncurses -L$HOME/local/lib" make
 cp tmux $HOME/local/bin
 cd ..
 
 # cleanup
-rm -rf $HOME/tmux_tmp
+[ -n $rmtempdir ] && echo "arg passed, removing $HOME/tmux_tmp" && rm -rfv $HOME/tmux_tmp || ls -ld $HOME/tmux_tmp
 
 echo "$HOME/local/bin/tmux is now available. You can optionally add $HOME/local/bin to your PATH."
